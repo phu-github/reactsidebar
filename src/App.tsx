1deb2019a1f0ac598ae7cf7b246a6b37865cdd12
phu-github <diemduyen05@gmail.com>
2022-09-27 21:34:30 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,99 @@ import './configs/global.scss';
 
 function App() {
 
-  const sidebarArr = [
-    {
-      name: 'Dashboard',
-      linkIcon: Images.InboxIcon || 'https://picsum.photos/19',
-      activeObject: false,
-      linkIconPlus: Images.InboxIcon,
-      imgVariantPlus: 'img-arrow',
-      path: '/inbox',
-    },
-    {
-      name: 'Actions',
-      linkIcon: Images.Book || 'https://picsum.photos/19',
-      activeObject: false,
-      linkIconPlus: Images.RightIcon,
-      imgVariantPlus: 'img-arrow',
-      path: '/action',
-    },
-    {
-      name: 'Reports',
-      linkIcon: Images.Grade || 'https://picsum.photos/19',
-      activeObject: false,
-      linkIconPlus: Images.ReportIcon,
-      imgVariantPlus: 'img-arrow',
-      path: '/reports',
-    }
-  ];
-  return (
-    <div className="App">
-      <SideBar sidebarArr={sidebarArr} />
-    </div>
-  );
+    const sidebarArr = [
+        {
+          name: 'Dashboard',
+          linkIcon: Images.Grade || 'https://picsum.photos/19',
+          activeObject: false,
+          linkIconPlus: Images.RightIcon,
+          imgVariantPlus: 'img-arrow',
+          path: '/dashboard',
+          childrens: [
+            {
+              name: 'Dashboard Sub1',
+              linkIcon: Images.BookMark || 'https://picsum.photos/19',
+              activeObject: false,
+              linkIconPlus: Images.RightIcon,
+              imgVariantPlus: 'img-arrow',
+              path: '/d-sub1'
+            },
+            {
+              name: 'Dashboard Sub2',
+              linkIcon: Images.BookMark || 'https://picsum.photos/19',
+              activeObject: false,
+              linkIconPlus: Images.RightIcon,
+              imgVariantPlus: 'img-arrow',
+              path: '/d-sub2'
+            },
+          ]
+        },
+        {
+          name: 'Inbox',
+          linkIcon: Images.InboxIcon || 'https://picsum.photos/19',
+          activeObject: false,
+          linkIconPlus: Images.InboxIcon,
+          imgVariantPlus: 'img-arrow',
+          path: '/inbox',
+        },
+        {
+          name: 'Actions',
+          linkIcon: Images.Book || 'https://picsum.photos/19',
+          activeObject: false,
+          linkIconPlus: Images.RightIcon,
+          imgVariantPlus: 'img-arrow',
+          path: '/action',
+          childrens: [
+            {
+              name: 'Action 1',
+              linkIcon: Images.BookMark || 'https://picsum.photos/19',
+              activeObject: false,
+              linkIconPlus: Images.RightIcon,
+              imgVariantPlus: 'img-arrow',
+              path: '/d-sub1'
+            },
+            {
+              name: 'Action 2',
+              linkIcon: Images.Grade || 'https://picsum.photos/19',
+              activeObject: false,
+              linkIconPlus: Images.RightIcon,
+              imgVariantPlus: 'img-arrow',
+              path: '/d-sub2',
+              childrens: [
+                {
+                  name: 'action 2.1',
+                  linkIcon: Images.BookMark || 'https://picsum.photos/19',
+                  activeObject: false,
+                  linkIconPlus: Images.RightIcon,
+                  imgVariantPlus: 'img-arrow',
+                  path: '/d-sub1'
+                },
+                {
+                  name: 'action 2.2',
+                  linkIcon: Images.BookMark || 'https://picsum.photos/19',
+                  activeObject: false,
+                  linkIconPlus: Images.RightIcon,
+                  imgVariantPlus: 'img-arrow',
+                  path: '/d-sub2'
+                },
+              ]
+            },
+          ]
+        },
+        {
+          name: 'Reports',
+          linkIcon: Images.Grade || 'https://picsum.photos/19',
+          activeObject: false,
+          linkIconPlus: Images.ReportIcon,
+          imgVariantPlus: 'img-arrow',
+          path: '/reports',
+        }
+    ];
+    return (
+        <div className="App">
+            <SideBar sidebarArr={sidebarArr} />
+        </div>
+    );
 }
 
 export default App;
